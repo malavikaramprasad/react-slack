9a2743c09c64d06af06f83834028d7241fa7f7bd
Malavika Ramprasad <31526040+malavikaramprasad@users.noreply.github.com>
2021-08-18 21:53:00 -0700

    Register form error handling

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,12 +1,12 @@
 // React imports
 import React from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // Firebase import
 import firebase from '../../firebase';
 
 // Semantic imports
-import { Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react'
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react'
 
 class Register extends React.Component {
 
@@ -14,7 +14,8 @@ class Register extends React.Component {
         username: '',
         email: '',
         password: '',
-        passwordConfirmation: ''
+        passwordConfirmation: '',
+        errors: [],
     }
 
     handleChange = (event) => {
@@ -22,19 +23,62 @@ class Register extends React.Component {
     }
 
     handleSubmit = event => {
-        event.preventDefault();
-        firebase
-        .auth()
-        .createUserWuthEmailAndPassword()
+        if(this.isFormValid()) {
+            event.preventDefault();
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.state.email, this.state.password)
+                .then(createdUser => {
+                    console.log(createdUser)
+                })
+                .catch(err => console.log(err))
+        }
     }
 
+    isFormValid = () => {
+        let errors = [];
+        let error;
+
+        if(this.isFormEmpty(this.state)) {
+            error = { message: 'Fill in all fields'};
+            this.setState({ errors: errors.concat(error)});
+            return false;
+
+        } else if(!this.isPasswordValid(this.state)) {
+            error = { message: 'Password is invalid'} ;
+            this.setState({ errors: errors.concat(error) });
+            return false;
+            
+        } else {
+            return true;
+        }
+    }
+
+    isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+        return !username.length || !email.length || !password.length || !passwordConfirmation.length;
+    }
+
+    isPasswordValid = ({ password, passwordConfirmation }) => {
+        if(password.length < 6 || passwordConfirmation.length < 6)
+            return false;
+        
+        else if(password !== passwordConfirmation)
+            return false;
+
+        else
+            return true;
+
+    }
+
+    displayErrors = (errors) => errors.map((error, i) => <p key={i}> {error.message} </p>)
+    
     render() {
-        const { username, email, password, passwordConfirmation  } = this.state;
+        const { username, email, password, passwordConfirmation, errors } = this.state;
 
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450}}>
-                    <Header  as="h2" icon="orange" textAlign="center">
+                    <Header as="h2" icon color="orange" textAlign="center">
                         <Icon name="puzzle piece" color="orange"/>
                         Register to Chat.
                     </Header>
@@ -53,6 +97,12 @@ class Register extends React.Component {
                             </Button>
                         </Segment>
                     </Form>
+                    {errors.length > 0 && (
+                        <Message error>
+                            <h3> errors </h3>
+                            {this.displayErrors(errors)}
+                        </Message>
+                    )}
                     <Message> Already a user? <Link to="/login"> Login</Link> </Message>
                 </Grid.Column>
             </Grid> 
