b816075adc799b931d654a271260c8cfb3713d7e
Malavika Ramprasad <31526040+malavikaramprasad@users.noreply.github.com>
2022-02-08 09:13:57 -0800

    login page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // React imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom';
+
+// Firebase imports
+import firebase from './firebase';
 
 // Local imports
 import App from './components/App';
@@ -13,15 +16,29 @@ import 'semantic-ui-css/semantic.min.css'
 
 import registerServiceWorker from './registerServiceWorker';
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </Switch>
-    </Router>
-)
+class Root extends React.Component {
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+    componentDidMount = () => {
+        firebase.auth().onAuthStateChanged( user => {
+            if(user){
+                this.props.history.push("/")
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        )
+    }
+    
+}
+
+const RootwithAuth = withRouter(Root)
+
+ReactDOM.render(<Router><RootwithAuth /></Router>, document.getElementById('root'));
 registerServiceWorker();
